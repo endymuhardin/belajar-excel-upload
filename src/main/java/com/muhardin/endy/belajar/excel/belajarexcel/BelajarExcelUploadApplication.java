3373f9a4c2b7ba5dfccdfdd8a6e793f709ae7228
Endy Muhardin <endy@muhardin.com>
2019-11-19 10:35:20 +0700

    rename

--- src/main/java/com/muhardin/endy/belajar/excel/belajarexcel/BelajarExcelUploadApplication.java
+++ src/main/java/com/muhardin/endy/belajar/excel/belajarexcel/BelajarExcelUploadApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BelajarExcelApplication {
+public class BelajarExcelUploadApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BelajarExcelApplication.class, args);
+		SpringApplication.run(BelajarExcelUploadApplication.class, args);
 	}
 
 }
