44caf830442209a61779c7ef860d4ec7f9f18915
Endy Muhardin <endymuhardin@Endys-MacBook-Pro.local>
2019-11-19 08:55:19 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/excel/belajarexcel/BelajarExcelApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.excel.belajarexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarExcelApplication.class, args);
+	}
+
+}
